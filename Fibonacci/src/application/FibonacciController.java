bedad4d77bbdeb7275b90d3502fb6e02ee0712c5
student <student@notebook1.cabinet1>
2018-10-30 12:54:31 +0200

    Реалізовано обчислення числа Фібоначчі.

--- Fibonacci/src/application/FibonacciController.java
+++ Fibonacci/src/application/FibonacciController.java
@@ -1,5 +1,26 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
 public class FibonacciController {
-	
+
+	@FXML
+	TextField input;
+	@FXML
+	Label result;
+
+	@FXML
+	public void calculate() {
+		long n = Long.parseLong(input.getText());
+		long a = 0, b = 0, c = 1;
+		for (int i = 0; i < n; i++) {
+			a = b;
+			b = c;
+			c = a + b;
+		}
+		result.setText(Long.toString(c));
+	}
+
 }
