0c788139fee4d118408001fad67f956c4038624b
protomors <protomors@gmail.com>
2019-07-20 01:37:05 +0300

    Проект і підпроект з використанням Maven.

---
+++ hello/src/main/java/atmp/javafx/App.java
@@ -0,0 +1,13 @@
+package atmp.javafx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
